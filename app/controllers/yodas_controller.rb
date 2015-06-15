a33f7734ccd79a787b575500cedf69a70ff6e0d4
abeilleabejorro <abeille.abejorro@gmail.com>
2015-06-15 10:00:28 -0400

    added voice

--- app/controllers/yodas_controller.rb
+++ app/controllers/yodas_controller.rb
@@ -22,28 +22,3 @@ class YodasController < ApplicationController
   end
 end
 
-
-
-# require 'unirest'
-# require 'nokogiri'
-# require 'pry'
-
-# class YodasController < ApplicationController
-
-#  skip_before_action :verify_authenticity_token
-
-#  def index
-#  end
-
-#  def create
-#    @input = params["words"]
-
-#     skywalker = Yoda.new(@input)
-#     result = skywalker.yoda_api
-
-#     skywalker.yoda_talks(result)
-#     redirect_to "/"
-
-#  end
-# end
-
